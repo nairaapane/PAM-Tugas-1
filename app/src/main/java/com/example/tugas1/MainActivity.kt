bf76a35a156adbfc0d50b524f4d20d1c2b7a4496
nairaapane <nairapane@student.ub.ac.id>
2024-08-25 11:08:03 +0700

    first commit

--- app/src/main/java/com/example/tugas1/MainActivity.kt
+++ app/src/main/java/com/example/tugas1/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.tugas1
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +12,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val nameAndNimTextView: TextView = findViewById(R.id.nameAndNimTextView)
+
+        val name = "Naira Oktarini Pane"
+        val nim = "225150407111040"
+
+        nameAndNimTextView.text = "Nama: $name\nNIM: $nim"
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
